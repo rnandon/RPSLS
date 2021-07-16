5d8db5fabcc26fbf2ffd1a45985f4b00271b828b
Ryan <ryan@ryannd.com>
2021-07-16 14:01:22 -0500

    Implemented basics of player class

--- player.py
+++ player.py
@@ -1,2 +1,16 @@
 class Player:
-    pass
+    def __init__(self):
+        self.current_gesture = None
+        self.total_wins = 0
+
+    def get_gesture(self):
+        return self.current_gesture
+
+    def set_gesture(self, gesture):
+        self.current_gesture = gesture
+
+    def get_wins(self):
+        return self.total_wins
+
+    def win_round(self):
+        self.total_wins += 1
